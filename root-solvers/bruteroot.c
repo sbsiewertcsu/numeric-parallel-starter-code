aca890d352586512832ba347b2519ac421b60f5e
Sam Siewert <69611220+sbsiewertcsu@users.noreply.github.com>
2023-04-19 07:24:22 -0700

    Improved search progress output
    Improved search progress output

--- root-solvers/bruteroot.c
+++ root-solvers/bruteroot.c
@@ -14,19 +14,23 @@
 // on an estimate of where the root is on an interval.
 //
 // Generally, Newton should converge fastest, followed by Regula Falsi, then Bisection, and the Bruteroot will
-// get the root stepping in one direction looking for a simple sign change.
+// get the root stepping in one direction looking for a simple sign change, but is the slowest.
+//
+// This code has an added outter do-while loop to find all roots on a larger interval.
 //
 #include <stdio.h>
 #include <math.h>
 #include <float.h>
 
-// model a function with desmos for example to "see" roots
+// First model a function with desmos.com for example to "see" roots
 // 
 // https://www.desmos.com/calculator
+//
 double f(double x)
 {
-    //return x*log10(x) - 1.2;
+    // Fill in your function here using the math library
 
+    //return x*log10(x) - 1.2;
     //return cos(x*x);
 
     // As can be seen with Desmos, this example equation has roots near: -2.851, 4.758, and 7.792
@@ -35,29 +39,32 @@ double f(double x)
 
 int main(void)
 {
-    int itr, maxitr, rootfound=0;
+    unsigned int itr, maxitr, rootfound=0, total_itr=0;
     double max_error=0.0;
-    double step=2.0*DBL_EPSILON, x0, x1, xf, sign, start;
+    double step=2.0*DBL_EPSILON, x0, x1, xfinal, sign_changed=0, start;
 
-    printf("\nEnter x0 start, xf end, step size and maximum iterations\n");
-    scanf("%lf %lf %lf %d", &x0, &xf, &step, &maxitr);
+    printf("\nEnter x0 start, xfinal end, step size and maximum iterations to search for each root on sub-interval of 1.0\n");
+    scanf("%lf %lf %lf %d", &x0, &xfinal, &step, &maxitr);
     start=x0;
 
-    printf("Will step at %lf from %lf to %lf to find root\n", step, x0, (maxitr*step));
+    printf("\nWill step at %lf from %lf to %lf to find each root, with %d total search steps\n\n",
+           step, x0, xfinal, (((unsigned)(xfinal-x0)+1)*maxitr));
+    printf("Starting search:\n");
 
-    do
+    do // while there are potentially more roots on the search interval
     {
-        for (itr=1; itr<=maxitr && (x0 < xf); itr++)
+        for (itr=1; (itr<=maxitr); itr++) // find next root on sub-interval
         {
 
             x1=x0+step;
 
-            // Any negative value x positive or vice versa will result in a sign less than zero - a crossing
-            sign = f(x0) * f(x1);
+            // Any difference in sign between x0 and x1 will result in a sign_changed less than zero - a crossing
+            sign_changed = f(x0) * f(x1);
 
-            if(sign < 0.0)
+            if(sign_changed < 0.0)
             {
-                printf("Sign change at %3d, estimated root at %20.15lf, f[root]=%20.15lf\n", itr, (x1+x0)/2.0, f((x1+x0)/2.0));
+                printf("\n*** Sign change at %3d iterations: estimated root at %16.15lf, f[root]=%16.15lf\n",
+                       total_itr+itr, (x1+x0)/2.0, f((x1+x0)/2.0));
                 if(fabs(f((x1+x0)/2.0)) > max_error) max_error=fabs(f((x1+x0)/2.0));
                 rootfound++;
 
@@ -65,22 +72,31 @@ int main(void)
                 break;
             }
 
-            x0=x1;
+            x0=x1; // advance the step interval
         }
 
-        // Start search for next root one step beyond last one found
+        // Re-start search for next root one step beyond last one found
+        total_itr += itr;
+
+        // Indicate searching progress
+        printf(".");
+        //printf("Continuing serach after %d total iterations\n", total_itr);
+
         x0=x1;
         itr=1;
 
-    } while((x0 <  xf) && itr < maxitr);
+    } while((x0 <  xfinal) && (itr < maxitr)); // exit outter loop search for all roots if end reached or max iterations
+
+    printf("\n\nSearch Results:\n");
 
     if(!rootfound)
     {
-        printf("After %d iterations: No solution (zero crossing) found on interval %lf to %lf\n", maxitr, start, x0);
+        printf("After %d iterations: No solution (zero crossing) found on interval %lf to %lf\n", itr, start, x0);
     }
     else
     {
-        printf("%d roots found on interval %lf to %lf, with max error=%lf\n", rootfound, start, x0, max_error);
+        printf("%d roots found on interval %lf to %lf, with max error=%lf in %d iterations\n",
+               rootfound, start, x0, max_error, total_itr);
     }
 
     return 1;
