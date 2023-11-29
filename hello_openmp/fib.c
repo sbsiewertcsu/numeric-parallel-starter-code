617df7bf513037e81df0a1d7874e7c3719cb3231
Sam Siewert <69611220+sbsiewertcsu@users.noreply.github.com>
2023-11-29 07:11:34 -0800

    Better documentation
    Better documentation including challenges of a fast fibonacci number calculator for a large range that runs in parallel faster than sequential

--- hello_openmp/fib.c
+++ hello_openmp/fib.c
@@ -1,6 +1,44 @@
+// Notes:
+//
+// https://www.mathsisfun.com/numbers/fibonacci-sequence.html
+//
+// The sequence is simple but has an inherent loop-carried dependency.  This makes it non-trivial to
+// speed-up with parallelism.
+//
+// Approaches to solve this include OpenMP tasking and/or dynamic programming strategies.  Geeks for geeks has documented
+// multiple coding methods including dynamic programming here:
+// * https://www.geeksforgeeks.org/cpp-program-for-fibonacci-numbers/
+//
+// Also, the n'th fibonacci can be a very large number that overflows a 64-bit unsigned integer, another interesting
+// complication of an otherwise simple series.
+//
+// For C++, one approach to dealing with large numbers is GMP - https://gmplib.org/ or using Boost:
+// * https://www.geeksforgeeks.org/generating-large-fibonacci-numbers-using-boost-library/
+// * https://codereview.stackexchange.com/questions/213101/calculate-huge-fibonacci-numbers-in-c-using-gmp-and-boostmultiprecision
+//
+// Large fibonacci numbers are interesting from a math theory viewpoint.  For example, "As of September 2023,
+// the largest known certain Fibonacci prime is F201107, with 42029 digits. It was proved prime by Maia Karpovich
+// in September 2023.[4] The largest known probable Fibonacci prime is F6530879. It was found by Ryan Propper in
+// August 2022.[2] - https://en.wikipedia.org/wiki/Fibonacci_prime
+//
+// Here's some lists of the first 10, 100, 300, and 500 fibonacci numbers:
+// * https://www.math.net/list-of-fibonacci-numbers
+// * https://planetmath.org/listoffibonaccinumbers
+//
+// Pacheco Textbook code for fibonacci examples:
+// * https://www.ecst.csuchico.edu/~sbsiewert/csci551/code/ipp2-source/ch5/
+// * https://www.ecst.csuchico.edu/~sbsiewert/csci551/code/ipp2-source/ch5/omp_fib.c
+// * https://www.ecst.csuchico.edu/~sbsiewert/csci551/code/ipp2-source/ch5/omp_fibo.c
+// * https://www.ecst.csuchico.edu/~sbsiewert/csci551/code/ipp2-source/ch5/omp_fib_time.c
+// * https://www.ecst.csuchico.edu/~sbsiewert/csci551/code/ipp2-source/ch5/omp_fib_broken1.c
+// * https://www.ecst.csuchico.edu/~sbsiewert/csci551/code/ipp2-source/ch5/omp_fib_broken2.c
+//
 #include <stdio.h>
 #include <time.h>
 #include <omp.h>
+#include <stdlib.h>
+
+#define WORKERS 1
 
 unsigned long long int fibc(unsigned int n)
 {
@@ -20,7 +58,7 @@ unsigned long long int fibc(unsigned int n)
 
 int main(int argc, char *argv[])
 {
-  unsigned int g_fibcnt=64;
+  unsigned int g_fibcnt=32;
   unsigned long long int g_fibnum;
 
   struct timespec start, end;
@@ -29,17 +67,24 @@ int main(int argc, char *argv[])
   if (argc == 2)
   {
       sscanf(argv[1], "%u", &g_fibcnt);
-      printf("will compute %uth Fibonacci number\n", g_fibcnt);
+      printf("will compute Fibonacci of %u\n", g_fibcnt);
   }
   else
-      printf("will compute %uth Fibonacci number\n", g_fibcnt);
+      printf("will compute Fibonacci of %u\n", g_fibcnt);
+
+  // Program range limitation based on unsigned 64-bit results
+  if(g_fibcnt > 93)
+  {
+      printf("Program cannot compute a fiboannci number greater than 32 - will overflow\n");
+      exit(-1);
+  }
 
 
-
-  clock_gettime(CLOCK_MONOTONIC, &start);
   // parallel BEGIN
+  clock_gettime(CLOCK_MONOTONIC, &start);
 
-# pragma omp parallel num_threads(4)
+// simple pragma may appear to work, but it does not divide work, it simply replicates it
+# pragma omp parallel num_threads(WORKERS)
   {
       unsigned int fibcnt=g_fibcnt;
       unsigned long long int fibnum;
@@ -49,8 +94,8 @@ int main(int argc, char *argv[])
       g_fibnum=fibnum;
   }
 
-  // parallel END
   clock_gettime(CLOCK_MONOTONIC, &end);
+  // parallel END
 
 
   fstart=start.tv_sec + (start.tv_nsec / 1000000000.0);
