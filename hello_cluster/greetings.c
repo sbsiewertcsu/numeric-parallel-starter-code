3dfa0034dfa39937e1a51961009bec86305f0749
Sam Siewert <69611220+sbsiewertcsu@users.noreply.github.com>
2024-06-13 12:47:28 -0700

    Fix to remove warning
    New compiler gripe - made string bigger!

--- hello_cluster/greetings.c
+++ hello_cluster/greetings.c
@@ -4,45 +4,46 @@
 
 #include <unistd.h>
 
-const int MAX_STRING = 100;
+const int MAX_STRING = 512;
 
 int main(void)
 {
-        char greeting[MAX_STRING];
-        char hostname[MAX_STRING];
-        char nodename[MPI_MAX_PROCESSOR_NAME];
-        int comm_sz;
-        int my_rank, namelen;
+    char greeting[MAX_STRING];
+    char hostname[MAX_STRING];
+    char nodename[MAX_STRING];
+    //char nodename[MPI_MAX_PROCESSOR_NAME];
+    int comm_sz;
+    int my_rank, namelen;
 
-        MPI_Init(NULL, NULL);
-        MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
-        MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+    MPI_Init(NULL, NULL);
+    MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 
-        if(my_rank != 0)
+    if(my_rank != 0)
+    {
+        gethostname(hostname, MAX_STRING);
+
+        MPI_Get_processor_name(nodename, &namelen);
+
+        sprintf(greeting, "Hello from process %d of %d on %s\n", my_rank, comm_sz, nodename);
+        MPI_Send(greeting, strlen(greeting)+1, MPI_CHAR, 0, 0, MPI_COMM_WORLD);
+    }
+    else
+    {
+        gethostname(hostname, MAX_STRING);
+
+        MPI_Get_processor_name(nodename, &namelen);
+
+        printf("Hello from process %d of %d on %s\n", my_rank, comm_sz, nodename);
+
+        for(int q=1; q < comm_sz; q++)
         {
-                gethostname(hostname, MAX_STRING);
-
-                MPI_Get_processor_name(nodename, &namelen);
-
-                sprintf(greeting, "Hello from process %d of %d on %s", my_rank, comm_sz, nodename);
-                MPI_Send(greeting, strlen(greeting)+1, MPI_CHAR, 0, 0, MPI_COMM_WORLD);
+                MPI_Recv(greeting, MAX_STRING, MPI_CHAR, q, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                printf("%s\n", greeting);
         }
-        else
-        {
-                gethostname(hostname, MAX_STRING);
+    }
 
-                MPI_Get_processor_name(nodename, &namelen);
-
-                printf("Hello from process %d of %d on %s", my_rank, comm_sz, nodename);
-
-                for(int q=1; q < comm_sz; q++)
-                {
-                        MPI_Recv(greeting, MAX_STRING, MPI_CHAR, q, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-                        printf("%s\n", greeting);
-                }
-        }
-
-        MPI_Finalize();
-        return 0;
+    MPI_Finalize();
+    return 0;
 }
 
