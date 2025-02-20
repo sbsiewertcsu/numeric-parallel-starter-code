664d4fd41d34b326476230395d3a9f7d16b1f028
Sam Siewert <69611220+sbsiewertcsu@users.noreply.github.com>
2025-02-20 15:45:02 -0800

    updated scripts
    updated scripts

--- hello_cluster/c1_hosts_test.sh
+++ hello_cluster/c1_hosts_test.sh
@@ -1,34 +1,65 @@
 #!/bin/sh
-ping -c 10 -w 3 o251-01
-ping -c 10 -w 3 o251-02
-ping -c 10 -w 3 o251-03
-ping -c 10 -w 3 o251-04
-ping -c 10 -w 3 o251-05
-ping -c 10 -w 3 o251-06
-ping -c 10 -w 3 o251-07
-ping -c 10 -w 3 o251-08
-ping -c 10 -w 3 o251-09
-ping -c 10 -w 3 o251-10
-ping -c 10 -w 3 o251-11
-ping -c 10 -w 3 o251-12
-ping -c 10 -w 3 o251-13
-ping -c 10 -w 3 o251-14
-ping -c 10 -w 3 o251-15
-ping -c 10 -w 3 o251-16
-ping -c 10 -w 3 o251-17
-ping -c 10 -w 3 o251-18
-ping -c 10 -w 3 o251-19
-ping -c 10 -w 3 o251-20
-ping -c 10 -w 3 o251-21
-ping -c 10 -w 3 o251-22
-ping -c 10 -w 3 o251-23
-ping -c 10 -w 3 o251-24
-ping -c 10 -w 3 o251-25
-ping -c 10 -w 3 o251-26
-ping -c 10 -w 3 o251-27
-ping -c 10 -w 3 o251-28
-ping -c 10 -w 3 o251-29
-ping -c 10 -w 3 o251-30
-ping -c 10 -w 3 o251-31
+ssh -q o251-01 exit
+echo "o251-01 has" $? "issues"
+ssh -q o251-02 exit
+echo "o251-02 has" $? "issues"
+ssh -q o251-03 exit
+echo "o251-03 has" $? "issues"
+ssh -q o251-04 exit
+echo "o251-04 has" $? "issues"
+ssh -q o251-05 exit
+echo "o251-05 has" $? "issues"
+ssh -q o251-06 exit
+echo "o251-06 has" $? "issues"
+ssh -q o251-07 exit
+echo "o251-07 has" $? "issues"
+ssh -q o251-08 exit
+echo "o251-08 has" $? "issues"
+ssh -q o251-09 exit
+echo "o251-09 has" $? "issues"
+ssh -q o251-10 exit
+echo "o251-10 has" $? "issues"
+ssh -q o251-11 exit
+echo "o251-11 has" $? "issues"
+ssh -q o251-12 exit
+echo "o251-12 has" $? "issues"
+ssh -q o251-13 exit
+echo "o251-13 has" $? "issues"
+ssh -q o251-14 exit
+echo "o251-14 has" $? "issues"
+ssh -q o251-15 exit
+echo "o251-15 has" $? "issues"
+ssh -q o251-16 exit
+echo "o251-16 has" $? "issues"
+ssh -q o251-17 exit
+echo "o251-17 has" $? "issues"
+ssh -q o251-18 exit
+echo "o251-18 has" $? "issues"
+#ssh -q o251-19 exit
+#echo "o251-19 has" $? "issues"
+ssh -q o251-20 exit
+echo "o251-20 has" $? "issues"
+ssh -q o251-21 exit
+echo "o251-21 has" $? "issues"
+ssh -q o251-22 exit
+echo "o251-22 has" $? "issues"
+ssh -q o251-23 exit
+echo "o251-23 has" $? "issues"
+ssh -q o251-24 exit
+echo "o251-24 has" $? "issues"
+ssh -q o251-25 exit
+echo "o251-25 has" $? "issues"
+ssh -q o251-26 exit
+echo "o251-26 has" $? "issues"
+ssh -q o251-27 exit
+echo "o251-27 has" $? "issues"
+ssh -q o251-28 exit
+echo "o251-28 has" $? "issues"
+ssh -q o251-29 exit
+echo "o251-29 has" $? "issues"
+ssh -q o251-30 exit
+echo "o251-30 has" $? "issues"
+ssh -q o251-31 exit
+echo "o251-31 has" $? "issues"
 
 sleep 3
--- hello_cluster/c2_hosts_test.sh
+++ hello_cluster/c2_hosts_test.sh
@@ -1,34 +1,65 @@
 #!/bin/sh
-ping -c 10 -w 3 o244-01
-ping -c 10 -w 3 o244-02
-ping -c 10 -w 3 o244-03
-ping -c 10 -w 3 o244-04
-ping -c 10 -w 3 o244-05
-ping -c 10 -w 3 o244-06
-ping -c 10 -w 3 o244-07
-ping -c 10 -w 3 o244-08
-ping -c 10 -w 3 o244-09
-ping -c 10 -w 3 o244-10
-ping -c 10 -w 3 o244-11
-ping -c 10 -w 3 o244-12
-ping -c 10 -w 3 o244-13
-ping -c 10 -w 3 o244-14
-ping -c 10 -w 3 o244-15
-ping -c 10 -w 3 o244-16
-ping -c 10 -w 3 o244-17
-ping -c 10 -w 3 o244-18
-ping -c 10 -w 3 o244-19
-ping -c 10 -w 3 o244-20
-ping -c 10 -w 3 o244-21
-ping -c 10 -w 3 o244-22
-ping -c 10 -w 3 o244-23
-ping -c 10 -w 3 o244-24
-ping -c 10 -w 3 o244-25
-ping -c 10 -w 3 o244-26
-ping -c 10 -w 3 o244-27
-ping -c 10 -w 3 o244-28
-ping -c 10 -w 3 o244-29
-ping -c 10 -w 3 o244-30
-ping -c 10 -w 3 o244-31
+ssh -q o244-01 exit
+echo "o244-01 has" $? "issues"
+ssh -q o244-02 exit
+echo "o244-02 has" $? "issues"
+ssh -q o244-03 exit
+echo "o244-03 has" $? "issues"
+ssh -q o244-04 exit
+echo "o244-04 has" $? "issues"
+ssh -q o244-05 exit
+echo "o244-05 has" $? "issues"
+ssh -q o244-06 exit
+echo "o244-06 has" $? "issues"
+ssh -q o244-07 exit
+echo "o244-07 has" $? "issues"
+ssh -q o244-08 exit
+echo "o244-08 has" $? "issues"
+ssh -q o244-09 exit
+echo "o244-09 has" $? "issues"
+ssh -q o244-10 exit
+echo "o244-10 has" $? "issues"
+ssh -q o244-11 exit
+echo "o244-11 has" $? "issues"
+ssh -q o244-12 exit
+echo "o244-12 has" $? "issues"
+ssh -q o244-13 exit
+echo "o244-13 has" $? "issues"
+ssh -q o244-14 exit
+echo "o244-14 has" $? "issues"
+ssh -q o244-15 exit
+echo "o244-15 has" $? "issues"
+ssh -q o244-16 exit
+echo "o244-16 has" $? "issues"
+ssh -q o244-17 exit
+echo "o244-17 has" $? "issues"
+ssh -q o244-18 exit
+echo "o244-18 has" $? "issues"
+ssh -q o244-19 exit
+echo "o244-19 has" $? "issues"
+ssh -q o244-20 exit
+echo "o244-20 has" $? "issues"
+ssh -q o244-21 exit
+echo "o244-21 has" $? "issues"
+ssh -q o244-22 exit
+echo "o244-22 has" $? "issues"
+ssh -q o244-23 exit
+echo "o244-23 has" $? "issues"
+ssh -q o244-24 exit
+echo "o244-24 has" $? "issues"
+ssh -q o244-25 exit
+echo "o244-25 has" $? "issues"
+ssh -q o244-26 exit
+echo "o244-26 has" $? "issues"
+ssh -q o244-27 exit
+echo "o244-27 has" $? "issues"
+ssh -q o244-28 exit
+echo "o244-28 has" $? "issues"
+ssh -q o244-29 exit
+echo "o244-29 has" $? "issues"
+ssh -q o244-30 exit
+echo "o244-30 has" $? "issues"
+ssh -q o244-31 exit
+echo "o244-31 has" $? "issues"
 
 sleep 3
