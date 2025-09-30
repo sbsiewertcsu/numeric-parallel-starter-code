4e9112278039fa9ce75556f4f5e2c55a5152f7b9
Sam Siewert <69611220+sbsiewertcsu@users.noreply.github.com>
2025-09-30 10:54:14 -0700

    Added quad double
    Added quad double

--- subtractive_cancel.c
+++ subtractive_cancel.c
@@ -1,4 +1,13 @@
+// Compile this with:
+//
+// gcc -std=gnu11 subtractive_cancel.c -o subcan -lquadmath
+//
+// gcc subtractive_cancel.c -o subcan -lm
+//
+// OR REMOVED QUAD DOUBLE on an older compiler
+//
 #include <stdio.h>
+#include <quadmath.h>  //include the for quad double testing
 
 // https://en.wikipedia.org/wiki/IEEE_754
 // https://en.wikipedia.org/wiki/Extended_precision#x86_extended_precision_format
@@ -6,6 +15,9 @@
 #define SINGLE_ITERATIONS 25 // IEEE 754 significand is 23 bits
 #define DOUBLE_ITERATIONS 55 // IEEE 754 significand is 52 bits
 #define LONG_DOUBLE_ITERATIONS 65 // IEEE 754 significand is 63 bits
+#define QUAD_DOUBLE_ITERATIONS 113 // IEEE 754 significand is 113 bits - https://en.wikipedia.org/wiki/Quadruple-precision_floating-point_format
+
+#define QUAD_TEST
 
 float single_test=0.0, single_addend=1.0;
 double double_test=0.0, double_addend=1.0;
@@ -15,6 +27,7 @@ int main(void)
 {
     int idx;
 
+    // FLOAT TEST for limit of Precision and mantissa size in bits
     for(idx=0; idx < SINGLE_ITERATIONS; idx++)
     {
         single_addend = single_addend / 2.0;
@@ -24,63 +37,93 @@ int main(void)
         // This test will succeed when 2^(-k) can't be represented
         if(single_test == 1.0)
         {
-                printf("SINGLE PRECISION digit limit found: ");
-                printf("LIMIT for 1+2^(-k)=1.0 @ k=%2.2d\n", idx+1);
+            printf("SINGLE PRECISION digit limit found: ");
+            printf("LIMIT for 1+2^(-k)=1.0 @ k=%2.2d\n", idx+1);
         }
         else
         {
-                printf("@ k=%2.2d, 1+2^(-k)=%.8f\n", idx+1, single_test);
+            printf("@ k=%2.2d, 1+2^(-k)=%.8f\n", idx+1, single_test);
         }
-
     }
+    printf("\n");
 
 
+    // DOUBLE TEST for limit of Precision and mantissa size in bits
     for(idx=0; idx < DOUBLE_ITERATIONS; idx++)
     {
-            double_addend = double_addend / 2.0;
-            double_test = 1.0 + double_addend;
+        double_addend = double_addend / 2.0;
+        double_test = 1.0 + double_addend;
 
-            // This test will succeed when 2^(-k) can't be represented
-            if(double_test == 1.0)
-            {
-                    printf("DOUBLE PRECISON digit limit found: ");
-                    printf("LIMIT for 1+2^(-k)=1.0 @ k=%2.2d\n", idx+1);
-            }
-            else
-            {
-                    printf("@ k=%2.2d, 1+2^(-k)=%.16f\n", idx+1, double_test);
-            }
-
+        // This test will succeed when 2^(-k) can't be represented
+        if(double_test == 1.0D)
+        {
+            printf("DOUBLE PRECISON digit limit found: ");
+            printf("LIMIT for 1+2^(-k)=1.0 @ k=%2.2d\n", idx+1);
+        }
+        else
+        {
+             printf("@ k=%2.2d, 1+2^(-k)=%.16f\n", idx+1, double_test);
+        }
     }
+    printf("\n");
 
 
+    // LONG DOUBLE TEST for limit of Precision and mantissa size in bits
     for(idx=0; idx < LONG_DOUBLE_ITERATIONS; idx++)
     {
-            long_double_addend = long_double_addend / 2.0;
-            long_double_test = 1.0 + long_double_addend;
+        long_double_addend = long_double_addend / 2.0;
+        long_double_test = 1.0 + long_double_addend;
 
-            // This test will succeed when 2^(-k) can't be represented
-            if(long_double_test == 1.0)
-            {
-                    printf("DOUBLE PRECISON digit limit found: ");
-                    printf("LIMIT for 1+2^(-k)=1.0 @ k=%2.2d\n", idx+1);
-            }
-            else
-
-            {
-                    printf("@ k=%2.2d, 1+2^(-k)=%.34Lf\n", idx+1, long_double_test);
-            }
-
+        // This test will succeed when 2^(-k) can't be represented
+        if(long_double_test == 1.0L)
+        {
+            printf("LONG DOUBLE PRECISON digit limit found: ");
+            printf("LIMIT for 1+2^(-k)=1.0 @ k=%2.2d\n", idx+1);
+        }
+        else
+        {
+            printf("@ k=%2.2d, 1+2^(-k)=%.34Lf\n", idx+1, long_double_test);
+        }
     }
+    printf("\n");
 
+#ifdef QUAD_TEST
+    // ADD QUAD DOUBLE TEST here
+    //
+    // Gemini: GCC provides the non-standard type __float128 for 128-bit quadruple-precision floating-point values
+    //
+    __float128 qdouble_addend=1.0Q, qdouble_test=0.0Q;
+    char buffer[128];
 
+    for(idx=0; idx < QUAD_DOUBLE_ITERATIONS; idx++)
+    {
+        qdouble_addend = qdouble_addend / 2.0Q;
+        qdouble_test = 1.0Q + qdouble_addend;
+
+        // This test will succeed when 2^(-k) can't be represented
+        if(qdouble_test == 1.0Q)
+        {
+            printf("QUAD DOUBLE PRECISON digit limit found: ");
+            printf("LIMIT for 1+2^(-k)=1.0 @ k=%2.2d\n", idx+1);
+        }
+        else
+        {
+            quadmath_snprintf(buffer, sizeof(buffer), "%.34Qf", qdouble_test);
+            printf("@ k=%2.2d, 1+2^(-k)=%s\n", idx+1, buffer);
+        }
+    }
+    printf("\n");
+#endif
+
+    // DOUBLE special case testing
+    //
     // Testing for proper support of special values NaN and Inf in IEEE Floating Point
     double zero=0.0, one=1.0;
 
     printf("Nan and Inf tests:\n");
     printf("zero/zero = %lf\n", zero/zero);
     printf("one/zero = %lf\n", one/zero);
-    printf("one/zero = %lf\n", -(zero/zero));
-    printf("one/zero = %lf\n", -(one/zero));
+    printf("-(zero/zero) = %lf\n", -(zero/zero));
+    printf("-(one/zero) = %lf\n", -(one/zero));
 
 }
