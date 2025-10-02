a143b1419dec4228054875dd8409809103db0dcc
Sam Siewert <69611220+sbsiewertcsu@users.noreply.github.com>
2025-10-02 01:23:36 -0700

    1 Hz and full interval parallel integration
    1 Hz and full interval parallel integration

--- Sim-parallel/simtrainideal_omp.c
+++ Sim-parallel/simtrainideal_omp.c
@@ -4,35 +4,16 @@
 #include <time.h>
 #include <omp.h>
 
-// For values between 1 second indexed data, use linear interpolation to determine profile value at any "t".
-//
-// Wikipedia - https://en.wikipedia.org/wiki/Linear_interpolation
-//
-// This assumes that the profile is "piecewise linear", which is accurate for linear or constant functions, and 
-// reasonably accurate for most non-linear functions over small intervals of 1 second.
-//
-// For high frequency non-linear functions spline and other more advanced curve fitting methods could be used, but
-// the piecewise linear assumption is sufficient for CSCI 551 for acceleration profiles given.
-//
-// The alternative to a look-up table with linear interpolation is direct modeling of a function, but this requires 
-// knowledge of the profile function as a linear, polynomial, or transcendental function or combination there-of, and
-// this may not be known.
-
 #include "ex3.h"
 //#include "ex4.h"
-//#include "const.h"
-//#include "sine.h"
 
-
-// table look-up for acceleration profile given and velocity profile determined
-double table_accel(int timeidx);
-double table_vel(int timeidx);
-
-// indirect generation of acceleration or velocity at any time with table interpolation
-double faccel(double time);
-double fvel(double time);
+// direct generation of acceleration at any time with math library and arithmetic
+double ex3_accel(double time);
+double ex3_vel(double time);
+double ex4_accel(double time);
 
 // Create velocity and position profiles (tables) the same size as acceleration profile
+double AccelProfile[sizeof(DefaultProfile) / sizeof(double)];
 double VelProfile[sizeof(DefaultProfile) / sizeof(double)];
 double PosProfile[sizeof(DefaultProfile) / sizeof(double)];
 
@@ -85,40 +66,48 @@ void main(int argc, char *argv[])
         PosProfile[idx]=0.0;
     }
 
-    // Left Riemann sum test to match spreadsheet with abstracted integration function
-    //
-    // Potential to speed up with OpenMP or Pthreads
-    //
-    printf("\n\nTHREADED INTEGRATOR FUNCTION: Left Riemann sum test for table with %d elements\n", tsize);
+    printf("\n\nTHREADED INTEGRATOR FUNCTION: test for duration %d seconds\n", tsize);
     clock_gettime(CLOCK_MONOTONIC, &start);
     VelStep=0.0; VelProfile[0]=VelStep;
     PosStep=0.0; PosProfile[0]=PosStep;
+    AccelProfile[0]=ex3_accel(0.0);
     double time_a, time_b;
 
+#ifdef PROFILE
     // Overall simulation table loop for time=0, to last time in model
     for(idx=0; idx < tsize-1; idx++)
     {
         time_a = (double)idx;
         time_b = (double)idx+1;
 
+        AccelProfile[idx+1]=ex3_accel(time_b);
+
         #pragma omp parallel num_threads(thread_count) reduction(+:VelStep)
-        //VelStep += Local_Riemann(time_a, time_b, steps_per_idx, faccel);
-        //VelStep += Local_Trap(time_a, time_b, steps_per_idx, faccel);
-        //VelStep += Local_Simpson(time_a, time_b, steps_per_idx, faccel);
-        VelStep += Local_RK4(time_a, time_b, steps_per_idx, faccel);
+        VelStep += Local_RK4(time_a, time_b, steps_per_idx, ex3_accel);
 
         //printf("VelStep=%lf at time=%lf\n", VelStep, time_b);
         VelProfile[idx+1]=VelStep;
 
         #pragma omp parallel num_threads(thread_count) reduction(+:PosStep)
-        //PosStep += Local_Riemann(time_a, time_b, steps_per_idx, fvel);
-        //PosStep += Local_Trap(time_a, time_b, steps_per_idx, fvel);
-        //PosStep += Local_Simpson(time_a, time_b, steps_per_idx, fvel);
-        PosStep += Local_RK4(time_a, time_b, steps_per_idx, fvel);
+        PosStep += Local_RK4(time_a, time_b, steps_per_idx, ex3_vel);
 
         //printf("PosStep=%lf at time=%lf\n", PosStep, time_b);
         PosProfile[idx+1]=PosStep;
     }
+#else
+
+    time_a = 0.0;
+    time_b = 1800.0;
+
+    #pragma omp parallel num_threads(thread_count) reduction(+:VelStep)
+    VelStep += Local_RK4(time_a, time_b, steps_per_idx, ex3_accel);
+    VelProfile[tsize-1]=VelStep;
+
+    #pragma omp parallel num_threads(thread_count) reduction(+:PosStep)
+    PosStep += Local_RK4(time_a, time_b, steps_per_idx, ex3_vel);
+    PosProfile[tsize-1]=PosStep;
+
+#endif
 
     clock_gettime(CLOCK_MONOTONIC, &end);
     fstart=start.tv_sec + (start.tv_nsec / 1000000000.0);
@@ -267,88 +256,75 @@ double Local_RK4(double a, double b, int n, double funct(double))
 }
 
 
-// Simple look-up in accleration profile array
-//
-// Added array bounds check for known size of train arrays
-//
-double table_accel(int timeidx)
+double ex3_accel(double time)
 {
-    long unsigned int tsize = sizeof(DefaultProfile) / sizeof(double);
+    // computation of time scale for 1800 seconds
+    static double tscale=1800.0/(2.0*M_PI);
+    // determined such that acceleration will peak to result in translation of 122,000.0 meters
+    static double ascale=0.2365893166123;
 
-    // Check array bounds for look-up table
-    if(timeidx > tsize)
+    return (sin(time/tscale)*ascale);
+}
+
+
+// determined based on known anti-derivative of ex4_accel function
+double ex3_vel(double time)
+{
+    // computation of time scale for 1800 seconds
+    static double tscale=1800.0/(2.0*M_PI);
+    // determined such that velocity will peak to result in translation of 122,000.0 meters
+    static double vscale=0.2365893166123*1800.0/(2.0*M_PI);
+
+    return ((-cos(time/tscale)+1)*vscale);
+}
+
+
+double ex4_accel(double time)
+{
+    // maximum acceleration and deceleration in m/s/s
+    static double peak_accel=0.2905;
+
+    // model each phase of the acceleration profile directly
+    
+    // linearly increasing acceleration by 0.01 m/s/s up to peak_accel
+    if(time >= 0.0 && time <= 100.0)
     {
-        printf("timeidx=%d exceeds table size = %lu and range %d to %lu\n", timeidx, tsize, 0, tsize-1);
-        exit(-1);
+        return ( (peak_accel/100.0)*time );
     }
 
-    return DefaultProfile[timeidx];
-}
-
-
-double table_vel(int timeidx)
-{
-    long unsigned int tsize = sizeof(VelProfile) / sizeof(double);
-
-    if(timeidx > tsize)
+    // constant acceleration at peak_accel
+    if(time > 100.0 && time <= 300.0)
     {
-        printf("timeidx=%d exceeds table size = %lu and range %d to %lu\n", timeidx, tsize, 0, tsize-1);
-        exit(-1);
+	return peak_accel;
+    }
+    
+    // linearly decreasing acceleration by -0.01 m/s/s to zero
+    if(time > 300.0 && time <= 400.0)
+    {
+        return ( peak_accel - ((peak_accel/100.0)*(time-300.0)) );
+    }
+   
+    // zero acceleration
+    if(time > 400.0 && time <= 1400.0)
+    {
+	return 0.0;
     }
 
-    return VelProfile[timeidx];
-}
-
-
-// Simple linear interpolation example for table_accel(t) for any floating point t value
-// for a table of accelerations that are 1 second apart in time, evenly spaced in time.
-//
-// accel[timeidx] <= accel[time] < accel[timeidx_next]
-//
-//
-double faccel(double time)
-{
-    // The timeidx is an index into the known acceleration profile at a time <= time of interest passed in
-    //
-    // Note that conversion to integer truncates double to next lowest integer value or floor(time)
-    //
-    int timeidx = (int)time;
-
-    // The timeidx_next is an index into the known acceleration profile at a time > time of interest passed in
-    //
-    // Note that the conversion to integer truncates double and the +1 is added for ceiling(time)
-    //
-    int timeidx_next = ((int)time)+1;
-
-    // delta_t = time of interest - time at known value < time
-    //
-    // For more general case
-    // double delta_t = (time - (double)((int)time)) / ((double)(timeidx_next - timeidx);
-    //
-    // If time in table is always 1 second apart, then we can simplify since (timeidx_next - timeidx) = 1.0 by definition here
-    double delta_t = time - (double)((int)time);
-
-    return ( 
-               // The accel[time] is a linear value between accel[timeidx] and accel[timeidx_next]
-               // 
-               // The accel[time] is a value that can be determined by the slope of the interval and accel[timedix] 
-               //
-               // I.e. accel[time] = accel[timeidx] + ( (accel[timeidx_next] - accel[timeidx]) / ((double)(timeidx_next - timeidx)) ) * delta_t
-               //
-               //      ((double)(timeidx_next - timeidx)) = 1.0
-               // 
-               //      accel[time] = accel[timeidx] + (accel[timeidx_next] - accel[timeidx]) * delta_t
-               //
-               table_accel(timeidx) + ( (table_accel(timeidx_next) - table_accel(timeidx)) * delta_t)
-           );
-}
-
-
-double fvel(double time)
-{
-    int timeidx = (int)time;
-    int timeidx_next = ((int)time)+1;
-    double delta_t = time - (double)((int)time);
-
-    return (table_vel(timeidx) + ( (table_vel(timeidx_next) - table_vel(timeidx)) * delta_t) );
+    // linearly decreasing acceleration by -0.01 m/s/s to -peak_accel
+    if(time > 1400.0 && time <= 1500.0)
+    {
+        return ( -((peak_accel/100.0)*(time-1400.0)) );
+    }
+    
+    // constant acceleration at -peak_accel
+    if(time > 1500.0 && time <= 1700.0)
+    {
+        return -peak_accel;	
+    }
+    
+    // linearly increasing acceleration by 0.01 up to zero
+    if(time > 1700.0 && time <= 1800.0)
+    {
+        return ( (peak_accel/100.0)*(time-1700.0) );
+    }
 }
