5712ad2ece957bc31cb325a524f5ae9646935bc4
Sam Siewert <69611220+sbsiewertcsu@users.noreply.github.com>
2025-10-08 17:29:50 -0700

    minimal pragma only example for openmp
    minimal pragma only example for openmp

---
+++ hello_openmp/hello_omp_simp.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void Hello_thread(void);
+
+int main(int argc, char *argv[])
+{
+    int thread_count;
+
+    if(argc < 2)
+        printf("usage: hello_omp <number threads>\n");
+    else
+    {
+        sscanf(argv[1], "%d", &thread_count);
+    }
+
+//#pragma omp parallel num_threads(thread_count)
+    Hello_thread();
+
+    return 0;
+}
+
+void Hello_thread(void)
+{
+    printf("Hello from OMP thread\n");
+}
