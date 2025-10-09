8a3fcae9fc5798ffbbfc356151af6f42eb93e34d
Sam Siewert <69611220+sbsiewertcsu@users.noreply.github.com>
2025-10-09 00:56:27 -0700

    turn on or off pragma in build
    turn on or off pragma in build

--- hello_openmp/hello_omp_simp.c
+++ hello_openmp/hello_omp_simp.c
@@ -5,7 +5,7 @@ void Hello_thread(void);
 
 int main(int argc, char *argv[])
 {
-    int thread_count;
+    int thread_count=1;
 
     if(argc < 2)
         printf("usage: hello_omp <number threads>\n");
@@ -14,7 +14,9 @@ int main(int argc, char *argv[])
         sscanf(argv[1], "%d", &thread_count);
     }
 
-//#pragma omp parallel num_threads(thread_count)
+#ifdef OMP_ENABLE
+#pragma omp parallel num_threads(thread_count)
+#endif
     Hello_thread();
 
     return 0;
